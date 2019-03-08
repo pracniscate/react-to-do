2ff25121dd212fd85e658731d2a3faf325cd6bc5
pracniscate <lorena.giddens@outlook.com>
2019-03-08 08:25:51 -0500

    create state & pass it tothe Todos component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,29 @@ import Todos from './Components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Wake up early',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Make nutritious breakfast',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Wash socks',
+        completed: false
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
