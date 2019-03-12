0bfad44cf1709749b1d4df9b56def3b0b2522f8b
pracniscate <lorena.giddens@outlook.com>
2019-03-12 06:32:23 -0400

    print title of todos in jsx

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 
 class Todos extends Component {
   render() {
     // from the state in App.js
+    // array of Todos
     console.log(this.props.todos);
     
-    return (
-      <div>
-        <h1>Todos</h1>
-      </div>
-    );
+    // loop through the array of Todos & output something (JSX)
+    // return something for each todo
+    return this.props.todos.map((todo) => (
+      <TodoItem todo={todo} />
+    ));
   }
 }
 
